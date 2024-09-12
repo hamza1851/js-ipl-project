c403a20638b0bcb7b38199cb8e9203618795cf46
hamza1851 <hamza.dev011@gmail.com>
2024-09-12 16:28:14 +0530

    Add write to file ability

--- src/server/test-questions/test-2-question.js
+++ src/server/test-questions/test-2-question.js
@@ -1,7 +1,11 @@
 import matchWonPerTeamPerYear from "../2-matches-won-per-team-per-year.js"
-import data from "../../public/output/matches.json" assert { type: "json" }
+import data from "../../public/output/parsedMatchesData.json" assert { type: "json" }
+import * as fs from "fs"
 
 console.log("---------------Questions-2---------------")
 
 const result = matchWonPerTeamPerYear(data)
+const json = JSON.stringify(result)
+console.log(json)
+fs.writeFileSync("../public/output/2-team-winning-stats.json", json)
 console.log(result)
