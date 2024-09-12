7334d34f9c74663343370fab55193608c96ddac8
hamza1851 <hamza.dev011@gmail.com>
2024-09-12 16:15:27 +0530

    Modidfied parser code

--- src/server/parce-deliveries-csv.js
+++ src/server/parce-deliveries-csv.js
@@ -4,13 +4,13 @@ import * as fs from "fs"
 const results = []
 
 // Using the csv-parser as a stream to read from the file and pushing data into the results array
-fs.createReadStream("../data/matches.csv")
+fs.createReadStream("../data/deliveries.csv")
   .pipe(csvParser())
   .on("data", (data) => results.push(data))
   .on("end", () => {
     // Once all data has been parsed, write to file
     fs.writeFileSync(
-      "../public/output/parsedMatchesData.json",
+      "../public/output/parsedDeliveriesData.json",
       JSON.stringify(results, null, 2)
     )
   })
